0ae6095bbf1694134e07a22445c9dcfd09c10a09
bpark <byungsun1819@gmail.com>
2023-08-10 20:05:04 -0700

    completed

--- src/App.js
+++ src/App.js
@@ -5,50 +5,70 @@ import { useState } from 'react';
 
 function App() {
 
-  let post = 'Vancouver Sushi Restaurants';
-  let [title, setTitle] = useState(['Sweatshirt', 'Pants', 'Shirt']);
+  let [title, setTitle] = useState(['Vancouver', 'Toronto', 'Montreal']);
   let [like, setLike] = useState([0, 0, 0]);
   let [modal, setModal] = useState(false);
+  let [titleStatus, setTitleStatus] = useState(0);
+  let [input, setInput] = useState("");
 
-  const changeModal = () => {
-    setModal(!modal);
+  const clickPost = () => {
+    let title_copy = [...title];
+    title_copy.unshift(input);
+    let like_copy = [...like];
+    like_copy.unshift(0);
+    setTitle(title_copy);
+    setLike(like_copy);
   }
+
 
   return (
     <div className="App">
       <div className="blog-nav">
-        <h4> BLOG </h4>
+        <h2> BLOG </h2>
       </div>
 
       {
         title.map((a, i) => {
           return (
             <div className="list" key={i}>
-              <h4> <span onClick={changeModal}>{ a }</span> <span onClick={() => {
+              <h3> <span onClick={()=>{ setModal(!modal); setTitleStatus(i) }}>{ a }</span> <span onClick={() => {
                 let copy = [...like];
                 copy[i] = copy[i] + 1;
                 setLike(copy);
-              }}>👍</span> { like[i] } </h4>
+              }}>👍</span> { like[i] } </h3>
               <p> June 30th, 2023 </p>
+              <button className='delete' onClick={()=>{
+                let title_copy = [...title];
+                title_copy.splice(i, 1);
+                let like_copy = [...like];
+                like_copy.splice(i, 1);
+                setTitle(title_copy);
+                setLike(like_copy)
+              }}> delete </button>
             </div>
           )
         })
       }
 
+      <input className="posting" onChange={(e)=>{
+        setInput(e.target.value)
+        }} /> 
+      <button className="post" onClick={clickPost}> post </button>
       {
-        modal == true ? <Modal/> : null
+        modal == true ? <Modal title={title} setTitle={setTitle} titleStatus={titleStatus}/> : null
       }
 
     </div>
   );
 }
 
-const Modal = () => {
+const Modal = (props) => {
   return (
     <div className='modal'>
-      <h4> Title </h4>
+      <h4> { props.title[props.titleStatus] } </h4>
       <p> Date </p>
       <p> Contents </p>
+      <button>Modify</button>
     </div>
   )
 }
